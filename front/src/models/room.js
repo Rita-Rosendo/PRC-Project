8330987b9cf5dc95fa2cdde2895c8e4e9249e993
Rita-Rosendo <61228653+Rita-Rosendo@users.noreply.github.com>
2021-06-28 18:05:40 +0100

    Delete front directory

--- front/src/models/room.js
+++
@@ -1,9 +0,0 @@
-export default class Patient {
-  constructor(belongs_to, room_amenities,room_features,room_type,url) {
-    this.belongs_to = belongs_to;
-    this.room_amenities = room_amenities;
-    this.room_features = room_features;
-    this.room_type = room_type;
-    this.url = url;
-  }
-}
